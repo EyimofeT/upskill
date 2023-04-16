7e52cd0892851c0937219eeaa1c393e6ba584c68
blessedmadukoma <blessedmadukoma@gmail.com>
2023-04-16 03:29:20 +0100

    fix error messages

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -1,15 +1,20 @@
 import express from "express";
 const router = express.Router();
-import { Login, Registrer,getUser,resetPassword } from "../../controller/auth.js";
-import {userRegister} from "../../middleware/userRegister.js";
-import {userLogin} from "../../middleware/userlogin.js";
+import {
+  Login,
+  Registrer,
+  getUser,
+  resetPassword,
+} from "../../controller/auth.js";
+import { userRegister } from "../../middleware/userRegister.js";
+import { userLogin } from "../../middleware/userlogin.js";
 router.use(express.json());
 
 //getting the registrer infomation
-router.post("/auth/register",userRegister,Registrer);
+router.post("/auth/register", userRegister, Registrer);
 
-router.post("/auth/login",userLogin,Login);
-router.get("/auth/getUser",getUser);
-router.post("/auth/resetpassword",resetPassword);
+router.post("/auth/login", userLogin, Login);
+router.get("/auth/getUser", getUser);
+router.post("/auth/resetpassword", resetPassword);
 
 export { router as authRouter };
