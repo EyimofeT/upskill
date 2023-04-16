c01e759f6ef8bdd306ab72b9545ee088f09afdbe
blessedmadukoma <blessedmadukoma@gmail.com>
2023-04-16 03:47:35 +0100

    fix mysql issue

--- src/config/con_db.js
+++ src/config/con_db.js
@@ -2,7 +2,7 @@ import mysql_con from "mysql";
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const con = mysql_con.createConnection({
+export const con = mysql_con.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
